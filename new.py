1faefff07fc8c43659e4774bbbdc3dc8849a21f8
Chamathka <chamathka15644@gmail.com>
2023-10-15 01:46:46 +0530

    Revert "Python code to calculates the sum of all even numbers from 1 to a specified number"
    This reverts commit 2a78e954b33fba06de4cc027ffc46b94beb361e7.

--- new.py
+++ new.py
@@ -1,17 +1,4 @@
-#Python code to generate random number
 import random
 
 random_number = random.randint(1, 10)
 print("Random number:", random_number)
-
-#Python code to calculates the sum of all even numbers from 1 to a specified number
-def sum_of_evens(n):
-    total = 0
-    for i in range(2, n + 1, 2):
-        total += i
-    return total
-
-# Replace 10 with the number up to which you want to calculate the sum of even numbers
-number = 10
-result = sum_of_evens(number)
-print(f"The sum of even numbers from 2 to {number} is {result}")
