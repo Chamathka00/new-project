4f5980cc8c4852e72a91bc08cfac3ebb6fcd6e17
Chamathka <chamathka15644@gmail.com>
2023-10-15 17:29:18 +0530

    Insert user details

---
+++ user-details.py
@@ -0,0 +1,13 @@
+# Get user details
+print("Enter your details")
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+email = input("Enter your email address: ")
+phone_number = input("Enter your phone number: ")
+
+# Display the user details
+print("\nUser Details:")
+print("Name:", name)
+print("Age:", age)
+print("Email:", email)
+print("Phone Number:", phone_number)
