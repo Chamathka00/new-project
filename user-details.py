62340a81a163124d2833d66615bc5b8ec2be1253
Chamathka <chamathka15644@gmail.com>
2023-10-15 18:12:47 +0530

    Added index_number and occupation to the user-details

--- user-details.py
+++ user-details.py
@@ -1,18 +1,22 @@
 # Get user details
 print("Enter your details")
 name = input("Enter your name: ")
+index_number = input("Enter your index number: ")
 age = input("Enter your age: ")
 email = input("Enter your email address: ")
 phone_number = input("Enter your phone number: ")
 school_name = input("Enter your school name: ")
 university_name = input("Enter your university name: ")
+occupation = input("Enter your occupation: ")
 
 
 # Display the user details
 print("\nUser Details:")
 print("Name:", name)
+print("Index Number:", index_number)
 print("Age:", age)
 print("Email:", email)
 print("Phone Number:", phone_number)
 print("School Name:", school_name)
 print("University Name:", university_name)
+print("Occupation:", occupation)
